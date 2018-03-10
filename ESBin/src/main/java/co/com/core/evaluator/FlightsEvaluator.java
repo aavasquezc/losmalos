46285cdbeda1dd0f3b4c7c5a22b29d57479ed4fb
Wilder Villarraga <hormigah@gmail.com>
2018-03-09 23:22:14 -0500

    fsda

--- ESBin/src/main/java/co/com/core/evaluator/FlightsEvaluator.java
+++ ESBin/src/main/java/co/com/core/evaluator/FlightsEvaluator.java
@@ -10,24 +10,8 @@ public class FlightsEvaluator {
 	
 	public boolean checkStatusFlight(final Exchange exchange) {
 		FlightLeg leg = exchange.getIn().getBody(FlightLeg.class);
-		
-		return !leg.getStatus().equals("COMPLETED");
-	}
-
-	public boolean checkIngate(final Exchange exchange) {
-		FlightLeg leg = exchange.getIn().getBody(FlightLeg.class);
-		//System.out.println("JAVIER");
-		//return leg.getStatus().equals("OUTGATE");
-		//return leg.getFlightAttributes().getStatus().equals("OUTGATE");
-
 		return true;
-	}
-	
-	public boolean checkOutgate(final Exchange exchange) {
-
-		FlightLeg leg = exchange.getIn().getBody(FlightLeg.class);
-		//return leg.getFlightAttributes().getStatus().equals("INGATE");
-		return true;
+//		return !leg.getStatus().value().equals("COMPLETED");
 	}
 
 }
--- ESBin/src/main/java/co/com/core/transformer/FlightsTransformer.java
+++ ESBin/src/main/java/co/com/core/transformer/FlightsTransformer.java
@@ -2,7 +2,9 @@ package co.com.core.transformer;
 
 import org.apache.camel.Exchange;
 import org.springframework.stereotype.Component;
-
+//
+//import co.com.canonical.flights.AlertOnAir;
+//import co.com.canonical.flights.DomainEvents;
 import co.com.canonical.flights.FlightLeg;
 
 @Component
@@ -13,10 +15,30 @@ public class FlightsTransformer {
 		FlightLeg leg = exchange.getIn().getBody(FlightLeg.class);
 		
 		StringBuilder xml = new StringBuilder();
-		xml.append("<flight-leg>");
-		xml.append("<EXTERNAL-KEY>");
-		xml.append(leg.getStatus());
-		xml.append("</EXTERNAL-KEY>");
+		xml.append("<flight-leg>\n");
+		
+		xml.append("<external-key>");
+		xml.append(leg.getExternalKey());
+		xml.append("</external-key>\n");
+//		xml.append("<status>");
+//		xml.append(leg.getStatus());
+//		xml.append("</status>\n");
+
+		xml.append("<domain-events>");
+//		
+//		DomainEvents domainEvents = leg.getDomainEvents();
+//		AlertOnAir alertOnAir = domainEvents.getAlertOnAir();
+//		String description = alertOnAir.getDescription();
+//		if(!description.isEmpty()) {
+//			xml.append("<alert-on-air>\n");
+//			xml.append("<description>\n");
+//			xml.append(description);
+//			xml.append("</description>\n");
+//			xml.append("</alert-on-air>\n");
+//		}
+//
+		xml.append("</domain-events>\n");
+		
 		xml.append("</flight-leg>");
 
 		exchange.getIn().setBody(xml.toString());
